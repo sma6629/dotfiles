3f3efd2b517d54ee18013a2353377f67dd0b2df4
sma6629 <mansib.syedahmed@gmail.com>
2025-08-11 21:54:35 -0400

    add more lsp-config key maps

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -42,6 +42,8 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "cd", vim.diagnostic.open_float, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 		end,
 	},
 }
